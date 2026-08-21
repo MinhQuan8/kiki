0d04953ec99375100324d37e1060773684e6d2eb
MinhQuan8 <103826665+MinhQuan8@users.noreply.github.com>
2026-08-21 22:21:48 +0700

    Delete test directory

--- test/su/su.js
+++
@@ -1,6 +0,0 @@
-// const fs = require("fs");
-// const { resolve } = require("path");
-
-// const content = fs.readFileSync(resolve(process.cwd(), "test/su/su.html"), { encoding: "utf8" });
-
-// console.log(content);
--- test/test.js
+++
@@ -1,9 +0,0 @@
-const food = { corn: '🌽', bacon: '🥓' };
-
-const cloneFood = Object.assign(food, {});
-
-cloneFood.bacon = "a"
-
-console.log(cloneFood); 
-console.log(food); 
-// { corn: '🌽', bacon: '🥓' }
